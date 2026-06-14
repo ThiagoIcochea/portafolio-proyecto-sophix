f6ca958c4cd04e940fc148ca8c9d5c54ed0ce03c
ThiagoIcochea <U22330428@utp.edu.pe>
2026-06-14 12:46:40 -0500

    Desplegando backend en microsoft con GitHub corrigiendo errores puerto

--- sophix-backend/src/main.ts
+++ sophix-backend/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
 
   console.log('3️⃣ Pipes set');
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 8080, '0.0.0.0');
 
   console.log('4️⃣ Server listening');
 }
