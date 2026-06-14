51f3e1a8d7e371fc6cfe800da4b227d94b16eecc
ThiagoIcochea <U22330428@utp.edu.pe>
2026-06-14 11:21:42 -0500

    Desplegando backend en microsoft con GitHub corrigiendo errores

--- sophix-backend/src/main.ts
+++ sophix-backend/src/main.ts
@@ -3,10 +3,19 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log('1️⃣ Starting app...');
+
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-  new ValidationPipe()
-);
+
+  console.log('2️⃣ App created');
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  console.log('3️⃣ Pipes set');
+
   await app.listen(process.env.PORT ?? 3000);
+
+  console.log('4️⃣ Server listening');
 }
 bootstrap();
+
